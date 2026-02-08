7ccbb0830d2722e94089be32db7281aadeb206d8
Li Pan <cn.jlu.lp.5212@gmail.com>
2026-02-08 17:04:48 +0800

    Add nvim config
    OSC52 for clipboard
    
    Signed-off-by: Li Pan <cn.jlu.lp.5212@gmail.com>

---
+++ nvim/init.lua
@@ -0,0 +1,29 @@
+vim.cmd.colorscheme("onenord")
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  -- use for Windows Terminal which not support osc52 paste
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+-- use for other terminal which support osc52 paste
+--  paste = {
+--    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+--    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+--  },
+}
+
