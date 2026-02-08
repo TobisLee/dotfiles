cc0c5cf5d68cc3cb9dfae4a70b923dc726927952
Li Pan <cn.jlu.lp.5212@gmail.com>
2026-02-08 18:05:37 +0800

    Add fish config
    Signed-off-by: Li Pan <cn.jlu.lp.5212@gmail.com>

---
+++ fish/conf.d/common_abbrs.fish
@@ -0,0 +1,12 @@
+abbr -a -- vim nvim
+abbr -a -- lg lazygit
+
+abbr -a -- ls 'eza -g'
+abbr -a -- la 'eza -lag --git --group'
+abbr -a -- ll 'eza -lg --git --group'
+abbr -a -- lt 'eza -lag --git --tree --level'
+
+# Tmux Abbrs
+abbr -a -- tls tmux ls
+abbr -a -- tnew tmux new -s
+abbr -a -- tatt tmux attach -t
---
+++ fish/config.fish
@@ -0,0 +1,1 @@
+source ~/.config/fish/common.fish
